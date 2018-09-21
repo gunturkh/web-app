4753d0b6b8cccbce6a72a25a79de8d827ceff61a
gunturkh <guntur.kh@gmail.com>
2018-09-21 15:26:44 +0700

    Remove unused component and add React Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import data from './assets/data.json'
-
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import BrowsePage from './components/pages/Browse.js'
+import ComparePage from './components/pages/Compare.js'
+import DetailPage from './components/pages/Detail.js'
+import Home from './components/pages/Home.js'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="row mt-5">
+					<Switch>
+						<Route exact path="/" component={Home}/>
+						<Route path="/browse" component={BrowsePage}/>
+						<Route path="/compare" component={ComparePage}/>
+						<Route path="/detail" component={DetailPage}/>
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
