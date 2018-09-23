3c3ecb23afe542e568896d9d45a65a8cd812eaa9
gunturkh <guntur.kh@gmail.com>
2018-09-23 21:04:05 +0700

    Create method to show compare after selecting 2 plans or more

--- src/components/pages/browse/BrowsePage.js
+++ src/components/pages/browse/BrowsePage.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react'
 import Navbar from '../../navbar/Navbar.js'
 import List from '../../list/List.js'
-import ButtonCompare from '../../buttonCompare/ButtonCompare.js'
+
 class Browse extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			compare: true,
+			checked: []
+		}
+
+		this.getCheckboxState = this.getCheckboxState.bind(this)
+	}
+
+	getCheckboxState(data,id,item){
+
+		if(data===true){
+			this.setState({
+				checked: [
+					...this.state.checked, 
+					{
+						id: id,
+						data: data,
+						item: item
+					}
+				]
+			})
+		}
+		else if(data===false){
+			let filtered = this.state.checked.filter(item =>{
+				return item.id!==id
+			})
+			
+			this.setState({
+				checked: filtered
+			})
+		}
+	}
 	render(){
+		console.log('State: ', this.state.checked)
+		
+		let compare = 'nav' + (this.state.checked.length<2 ? ' hidden' : '' )
+		
 		return (
 			<div>
-				<Navbar/>
-				<ButtonCompare/>
-				<List/>
+				<Navbar compare={compare} compareOnClick={this.state.checked}/>
+				<List getCheckboxState={this.getCheckboxState}/>
 			</div>
 		)
 	}
