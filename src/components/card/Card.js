bfe037e608eb4eabf629f07d96dd31063b043809
gunturkh <guntur.kh@gmail.com>
2018-09-23 14:24:56 +0700

    Create Card Component

---
+++ src/components/card/Card.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import ProviderLogo from '../providerlogo/ProviderLogo.js'
+import  './Card.css'
+import { Link } from 'react-router-dom'
+
+
+class Card extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			compare: false,
+		}
+		this.handleInputChange=this.handleInputChange.bind(this)
+	}
+    
+    
+	handleInputChange(event) {
+		const target = event.target
+		const value = target.type === 'checkbox' ? target.checked : target.value
+		const name = target.name
+		this.props.onChange(this.props.id)
+		this.setState({
+			[name]: value
+		})
+	}
+
+	render(){
+		let checked = 'card ' + (this.state.compare ? 'checked' : '')
+        
+		return(
+			<div className={checked}>
+				<div className="compare">
+					<input
+						key={this.props.id} 
+						className="checkbox-compare" 
+						type="checkbox" 
+						name="compare"
+						checked={this.props.checked}
+						onChange={this.handleInputChange}
+						disabled={this.props.disabled}
+					></input>
+				</div>
+
+				<div className="container">
+					<ProviderLogo logo={this.props.insuranceProviderId}/>
+					{this.props.insuranceProviderId}
+					<div>
+				Name: {this.props.name}
+					</div>
+					<div>
+				Sum: {this.props.sum}
+					</div>
+					<div>
+				Premium: {this.props.premium}
+					</div>
+					<div>
+						<Link to={'/detail'}>	<button onClick={() => this.props.onClick(this.props.item,this.props.id)} className="view-detail">View Detail</button> </Link>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Card
