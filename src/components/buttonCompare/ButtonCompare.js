22e1633cfdb564fba5897e83c82b3c8ada8a33e4
gunturkh <guntur.kh@gmail.com>
2018-09-23 14:58:30 +0700

    Add styling to ButtonCompare

--- src/components/buttonCompare/ButtonCompare.js
+++ src/components/buttonCompare/ButtonCompare.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import './ButtonCompare.css'
 class ButtonCompare extends Component{
 
 	render(){
 		return(
-			<button>Compare</button>
+			<button className="button-compare">Compare</button>
 		)
 	}
 }
