3a7e7977494d4392965013e1477c50331e9d3acd
gunturkh <guntur.kh@gmail.com>
2018-09-23 14:57:46 +0700

    Move BrowsePage to component>browse folder

--- src/components/pages/browse/BrowsePage.js
+++ src/components/pages/browse/BrowsePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import Navbar from '../navbar/Navbar.js'
-import List from '../list/List.js'
+import Navbar from '../../navbar/Navbar.js'
+import List from '../../list/List.js'
+import ButtonCompare from '../../buttonCompare/ButtonCompare.js'
 class Browse extends Component {
 	render(){
 		return (
 			<div>
 				<Navbar/>
+				<ButtonCompare/>
 				<List/>
 			</div>
 		)
