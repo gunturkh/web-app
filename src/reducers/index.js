81204eb99959babf98025583f16118b74b10dcba
gunturkh <guntur.kh@gmail.com>
2018-09-23 14:18:15 +0700

    Create Reducer - GET_CONTENT -SELECT_PLAN -COMPARE_PLAN

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,23 @@
 import data from '../assets/data.json'
+import { GET_CONTENT,SELECT_PLAN, COMPARE_PLAN } from '../constants/action-types'
 
 const initialState = {
-	content: data.content
+	content: data.content,
+	detail: {},
+	compare:[]
 }
 
 const rootReducer = (state = initialState, action) => {
 	switch (action.type) {
+	case GET_CONTENT:
+		return action.payload
+	case SELECT_PLAN: 
+		return {...state, detail:action.payload}
+	case COMPARE_PLAN:
+		return {
+			...state,
+			compare: [...state.compare, action.payload]
+		}
 	default: 
 		return state
 	}
