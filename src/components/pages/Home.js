e0a04454395ffe4eaf0f5954fda1d0d7ef68455a
gunturkh <guntur.kh@gmail.com>
2018-09-23 14:20:20 +0700

    Add Navbar component inside Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Navbar from '../navbar/Navbar.js'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
 	render(){
 		return (
 			<div>
+				<Navbar/>
 				<h1>Home</h1>
 				<li>
 					<Link to="/">Home</Link>
