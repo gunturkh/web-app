9d4dcf0b2caaffeb365b4859d10eb4c0627b38c5
gunturkh <guntur.kh@gmail.com>
2018-09-24 23:57:53 +0700

    Update navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,20 +3,16 @@ import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 class Navbar extends Component{
-	// constructor(props){
-	// 	super(props)
-	// }
-
 	render(){
 		return (
-			<nav className="flexContainer Background">
-				<ul className="nav flexItem flexStart">
-					<NavLink to="/">Web App</NavLink>
+			<nav className="flexContainer Background front">
+				<ul className="nav flexItem">
+					<NavLink className="navBrand" to="/"><b>Web App</b></NavLink>
 				</ul>
 				<ul className="nav flexContainer flexEnd">
 					<NavLink to="/">Home</NavLink>
 					<NavLink to="/browse">Browse</NavLink>
-					<NavLink to="/compare" className={this.props.compare} onClick={()=>{console.log(this.props.compareOnClick)}}>Compare</NavLink>
+					<NavLink to="/compare" className={this.props.compare} onClick={this.props.compareOnClick}>Compare</NavLink>
 					
 				</ul>
 			</nav>
