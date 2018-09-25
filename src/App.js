71537edfb6501b14f7b0d13f08054c4400939c2d
gunturkh <guntur.kh@gmail.com>
2018-09-25 11:23:46 +0700

    Change route home to browse page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import BrowsePage from './components/pages/browse/BrowsePage.js'
 import ComparePage from './components/pages/compare/ComparePage.js'
 import DetailPage from './components/pages/detail/DetailPage'
-import Home from './components/pages/home/Home.js'
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="row mt-5">
 					<Switch>
-						<Route exact path="/" component={Home}/>
-						<Route path="/browse" component={BrowsePage}/>
+						<Route exact path="/" component={BrowsePage}/>
 						<Route path="/compare" component={ComparePage}/>
 						<Route path="/detail" component={DetailPage}/>
 					</Switch>
