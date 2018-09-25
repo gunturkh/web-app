44e6a445fc2a42449102cd0a90331fd75fe9a168
gunturkh <guntur.kh@gmail.com>
2018-09-25 11:42:47 +0700

    Remove unused class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import BrowsePage from './components/pages/browse/BrowsePage.js'
-import ComparePage from './components/pages/compare/ComparePage.js'
-import DetailPage from './components/pages/detail/DetailPage'
+import BrowsePage from './components/Pages/Browse/BrowsePage.js'
+import ComparePage from './components/Pages/Compare/ComparePage.js'
+import DetailPage from './components/Pages/Detail/DetailPage'
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div className="row mt-5">
+				<div>
 					<Switch>
 						<Route exact path="/" component={BrowsePage}/>
 						<Route path="/compare" component={ComparePage}/>
