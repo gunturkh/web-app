76e7bd3835e3b621cbed3fc9c639cbab6b2a4f2a
gunturkh <guntur.kh@gmail.com>
2018-09-25 11:27:21 +0700

    Styling DetailPage component

--- src/components/pages/detail/DetailPage.js
+++ src/components/pages/detail/DetailPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from '../../navbar/Navbar.js'
 import { connect } from 'react-redux'
+import CurrencyFormat from 'react-currency-format'
 import './DetailPage.css'
 import HDFCErgo from '../../../assets/HDFCErgo.png'
 import Religare from '../../../assets/Religare.png'
@@ -49,27 +50,34 @@ class Detail extends Component {
 
 
 	render(){
-		console.log('Detail : ', this.props.detail)
 		const detail = this.props.detail
 		const MedicalFeatures = detail.MedicalFeatures
 		const TravelFeatures = detail.TravelFeatures
-		const insuranceName = this.insuranceName(detail.insuranceProviderId)
-		const sumInsureds = detail.sumInsureds
+		const insuranceName = detail.insuranceProviderName //this.insuranceName(detail.insuranceProviderId)
+		const sumInsureds = Number(detail.sumInsureds)
 		const planName = detail.planName
 		const premium = detail.Premium
 		const logo = this.logoPicker(this.props.detail.insuranceProviderId)
 		
 		return (
-			<div className="flexContainer flexColumn fullHeight">
+			<div className="detailContainer">
 				<Navbar compare="nav hidden"/>
-				<div className="flexContainer flexRow flexSpaceAround">
-					<img src={logo} alt="insurance-logo" className="insurance-logo"></img>
-					<div>{insuranceName}</div>
-					<div>{premium}</div>
-					<div>{planName}</div>
-					<div>Sum Insureds: {sumInsureds}</div>
-					<div>{MedicalFeatures.map((item,id)=>(<div key={id}>{item.benefitName}</div>))}</div>
-					<div>{TravelFeatures.map((item,id)=>(<div key={id}>{item.benefitName}</div>))}</div>
+				<div className="detailWrapper">
+					<div className="card-detail">
+						<img src={logo} alt="insurance-logo" className="insurance-logo"></img>
+						<div className="detail-info-name"><b>{planName}</b></div>
+						<div className="detail-info-space"><b>By :</b> {insuranceName}</div>
+						<div className="detail-info-space"><b>Premium :</b> <CurrencyFormat value={premium}  displayType={'text'} thousandSeparator={true} prefix={'$'}/> </div>
+						<div className="detail-info-space"><b>Sum Insureds :</b> <CurrencyFormat value={sumInsureds}  displayType={'text'} thousandSeparator={true} prefix={'$'}/> </div>
+					</div>
+					<div className="card-detail-features">
+						<div className="detail-info"> 
+							<div className="detail-info-space "><b className="title">Medical Features :</b> {MedicalFeatures.map((item,id)=>(<div key={id}><li className="feature">{item.benefitName}</li></div>))}</div>
+						</div>
+						<div className="detail-info">
+							<div className="detail-info-space "><b className="title">Travel Features :</b> {TravelFeatures.map((item,id)=>(<div key={id}><li className="feature">{item.benefitName}</li></div>))}</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
